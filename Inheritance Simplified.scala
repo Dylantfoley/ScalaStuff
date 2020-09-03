f38755383f565ab89b088d53f4adafa781a9e7b6
Dylantfoley <49566150+Dylantfoley@users.noreply.github.com>
2020-09-03 13:10:14 -0400

    Add files via upload

---
+++ Inheritance Simplified.scala
@@ -0,0 +1,18 @@
+package playground
+
+object scalaPlayground extends App{
+  class class1 {
+    def hello(name: String): Unit = println(s"Hello $name, I am from class 1")
+  }
+  class class2 extends class1 with trait1 {
+    def hello2(name: String): Unit = println(s"Hello $name, I am from class 2")
+  }
+  trait trait1 {
+    def hello3(name:String): Unit = println(s"Hello $name, I am from trait1")
+  }
+  val greetings = new class2
+  greetings.hello("Dylan")
+  greetings.hello2("Dylan")
+  greetings.hello3("Dylan")
+
+}
